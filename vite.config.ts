e7facecdf51a085606493c382714258ede120ebb
yuliagusmy <yuliagusmy@gmail.com>
2025-05-22 01:18:08 +0800

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Pixel Pomodoro',
+        short_name: 'Pomodoro',
+        description: 'Pixel-art Pomodoro Timer',
+        theme_color: '#f7e6c4',
+        background_color: '#f7e6c4',
+        display: 'standalone',
+        start_url: '.',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
